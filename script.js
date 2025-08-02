be0b0ad9fe653a3c383f2531ba0999dca6f1d7f7
RamyGarici <ramymanilgarici@gmail.com>
2025-08-02 18:46:12 +0100

    Game logic finished

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 
-let playImage = document.getElementById("player-img")
-let computerImage = document.getElementById("computer-img")
-let winMessage = document.getElementById("win-msg")
+let playImage = document.getElementById("player-img");
+let computerImage = document.getElementById("computer-img");
+let winMessage = document.getElementById("win-msg");
+let playerScore = document.getElementById("player-score");
+let computerScore = document.getElementById("computer-score");
+let playerPoints = 0;
+let computerPoints = 0;
 
 
 function getComputerChoice(choice){
@@ -24,18 +28,58 @@ function getComputerChoice(choice){
 
 
     function getWinner(play,comp){
+      if (playerPoints >= 5 ) {
+               winMessage.innerText = "Player Won The Game!"
+               disableButtons();
+               return;
+  }
+    else if(computerPoints >= 5){
+        winMessage.innerText = "Computer Won The Game!"
+        disableButtons();
+        return;
+    }
+      
       if (play== "rock" && comp=="scissors" ){ winMessage.innerText = "You Won!";
+        playerPoints +=1;
+        playerScore.textContent = playerPoints;
+
 
       }
-      else if (play=="scissors" && comp=="rock"){winMessage.innerText = "You Lose";}
-      else if(play=="rock" && comp=="paper"){ winMessage.innerText = "You Lose";}
+      else if (play=="scissors" && comp=="rock"){winMessage.innerText = "You Lose";
+        computerPoints +=1;
+        computerScore.textContent = computerPoints;
+      }
+      
+      else if(play=="rock" && comp=="paper"){ winMessage.innerText = "You Lose";
+         computerPoints +=1;
+         computerScore.textContent = computerPoints;
+      }
       else if(play==comp){winMessage.innerText = "Draw";}
-      else if (play=="paper" && comp=="scissors"){winMessage.innerText = "You Lose";}
-      else if (play=="paper" && comp=="rock"){winMessage.innerText = "You Lose";}
-      else if (play=="scissors" && comp=="paper"){winMessage.innerText = "You Won!";}
+      else if (play=="paper" && comp=="scissors"){winMessage.innerText = "You Lose";
+         computerPoints +=1;
+         computerScore.textContent = computerPoints;
+      }
+      
+      else if (play=="paper" && comp=="rock"){winMessage.innerText = "You Lose";
+         computerPoints +=1;
+         computerScore.textContent = computerPoints;
+      }
+      else if (play=="scissors" && comp=="paper"){winMessage.innerText = "You Won!";
+         playerPoints +=1;
+         playerScore.textContent = playerPoints;
+      }
+  
     }
 
 
 
    
- 
+ function disableButtons(){
+    let buttons = document.querySelectorAll("button")
+    buttons.forEach(button => {
+        button.disabled = true;
+        button.style.opacity = 0.5;
+
+    })
+ }
+  
